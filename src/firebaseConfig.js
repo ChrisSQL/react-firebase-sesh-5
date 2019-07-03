80eb94cf2bdd184f2b5578e0c29f42e6b51ba9c3
Chris Maher <chrismaher.wit@gmail.com>
2019-07-03 16:07:04 +0100

    Initial commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCHRLOibxIQtzGaZ7Pi4SFaxFyOqp0H7oU",
+    authDomain: "onthesesh.ie",
+    databaseURL: "https://tnteventsapp.firebaseio.com",
+    projectId: "TNTeventsapp",
+    storageBucket: "tnteventsapp.appspot.com",
+    messagingSenderId: "37018668289"
+};
